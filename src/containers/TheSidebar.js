2d049bd1dbdf2fc81d1dfe831586c22a8c869cf4
underline84 <evanfatu@hotmail.com>
2022-10-18 07:11:27 -0300

    first commit

---
+++ src/containers/TheSidebar.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import { useSelector} from 'react-redux'
+import {
+  CCreateElement,
+  CSidebar,
+  CSidebarBrand,
+  CSidebarNav,
+  CSidebarNavTitle,
+  CSidebarMinimizer,
+  CSidebarNavItem,
+} from '@coreui/react'
+
+import CIcon from '@coreui/icons-react'
+
+// sidebar nav config
+import navigation from './_nav'
+
+const TheSidebar = () => {
+  const show = useSelector(state => state.sidebarShow)
+
+  return (
+    <CSidebar>
+      <CSidebarBrand className="d-md-down-none" to="/">
+        <CIcon
+          className="c-sidebar-brand-full"
+          name="logo-negative"
+          height={35}
+        />
+        <CIcon
+          className="c-sidebar-brand-minimized"
+          name="sygnet"
+          height={35}
+        />
+      </CSidebarBrand>
+      <CSidebarNav>
+
+        <CCreateElement
+          items={navigation}
+          components={{
+            CSidebarNavItem,
+            CSidebarNavTitle
+          }}
+        />
+      </CSidebarNav>
+      <CSidebarMinimizer className="c-d-md-down-none"/>
+    </CSidebar>
+  )
+}
+
+export default React.memo(TheSidebar)
